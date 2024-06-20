e2ca77715e9d2e6d57e0d03e9737444c0cef46f9
Love Divine <stephen.asiedu17@gmail.com>
2024-06-20 01:22:22 -0400

    python_dictionaries_assignments update

--- python_dictionaries_assignments.py
+++ python_dictionaries_assignments.py
@@ -17,3 +17,25 @@ print(beverage_menu)
 # the following code is not working, I am not sure why:
 # beverage_menu = restaurant_menu.update({"Soft Drinks": {"100% Pinneaple juice : 2.99", "100% Orange juice : 2.99"}})
 # print(beverage_menu)  this 
+
+
+service_tickets = {
+    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
+    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
+}
+
+#  Demonstrate your ability to use nested collections and loops 
+#  by creating a system to track customer service tickets.
+
+# Problem Statement: Develop a program that:
+
+# Tracks customer service tickets, each with a unique ID, customer name, issue description, and status (open/closed).
+# Implement functions to:
+# Open a new service ticket.
+# Update the status of an existing ticket  to "closed".
+# Display all tickets.
+#   (Bonus) filter by status
+# Initialize with some sample tickets and include functionality for additional ticket entry.
+
+
+for ti
