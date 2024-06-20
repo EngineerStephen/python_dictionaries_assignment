949d489d0d4a8dc27276c9577a1bcb1682246146
Love Divine <stephen.asiedu17@gmail.com>
2024-06-20 01:23:29 -0400

    python_dictionaries_assignments updated

--- python_dictionaries_assignments.py
+++ python_dictionaries_assignments.py
@@ -1,28 +1,3 @@
-restaurant_menu = {
-    "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
-    "Main Course": {"Steak": 15.99, "Salmon": 13.99},
-    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
-}
-
-print()
-print(restaurant_menu)
-print()
-# - Add a new category called "Beverages" with at least two items. 
-# - Update the price of "Steak" to 17.99. 
-# - Remove "Bruschetta" from "Starters". ---
-
-beverage_menu = restaurant_menu.update({"Soft Drinks": {"100% Pinneaple juice : 2.99", "100% Orange juice : 2.99"}})
-print(beverage_menu)
-
-# the following code is not working, I am not sure why:
-# beverage_menu = restaurant_menu.update({"Soft Drinks": {"100% Pinneaple juice : 2.99", "100% Orange juice : 2.99"}})
-# print(beverage_menu)  this 
-
-
-service_tickets = {
-    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
-    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
-}
 
 #  Demonstrate your ability to use nested collections and loops 
 #  by creating a system to track customer service tickets.
@@ -38,4 +13,22 @@ service_tickets = {
 # Initialize with some sample tickets and include functionality for additional ticket entry.
 
 
-for ti
+service_tickets = {
+    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
+    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
+}
+
+# Tracking customer service tickets, each with a unique ID, customer name, issue description, and status (open/closed).
+alice_ticket = service_tickets["Ticket001"]
+bob_ticket = service_tickets["Ticket002"]   
+print()
+# print(alice_ticket)
+# print(bob_ticket)
+print("Here are the organized tickets Tracking customer service tickets, each with a unique ID, customer name, issue description, and status (open/closed):    ")
+print()
+for detail in alice_ticket:
+    print(detail,":", alice_ticket[detail])
+print()
+print()
+for detail in bob_ticket:
+    print(detail,":", bob_ticket[detail])
